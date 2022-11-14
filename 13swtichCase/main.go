9abae9450e18bc2378e70f350850ef7eff19d611
Karan Singh Thakur <karanthakur.work@gmail.com>
2022-11-14 18:51:19 +0530

    added files

---
+++ 13swtichCase/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	fmt.Println("Switch and case in Golang")
+
+	rand.Seed(time.Now().UnixNano())
+	diceNumber := rand.Intn(6)
+	fmt.Println("Value of Dice is : ", diceNumber)
+
+	switch diceNumber {
+	case 1:
+		fmt.Println("Dice value is 1 and you can  open ")
+
+	case 2:
+		fmt.Println("You can move 2 spot")
+
+	case 3:
+		fmt.Println("You can move to 3 spot")
+
+	case 4:
+		fmt.Println("YOu can move to 4 spot")
+		fallthrough
+	case 5:
+		fmt.Println("You can move to 5 spot")
+
+	case 6:
+		fmt.Println("YOu can move to 6 spot and roll dice again")
+
+	default:
+		fmt.Println("What was that")
+	}
+
+}
