8ba5c3b72570fecfc7ca52b39cad3636c02131ff
Karan Singh Thakur <karanthakur.work@gmail.com>
2022-11-20 23:58:06 +0530

    added files

---
+++ 20urls/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+)
+
+const myurl string = "https://courses.learncodeonline.in/learn/home/Go-with-Golang/section/233285/lesson/1366689"
+
+func main() {
+	fmt.Println("Welcome  to Handling URLs in Golang ")
+
+	fmt.Println(myurl)
+
+	//parsing
+	result, _ := url.Parse(myurl)
+	fmt.Println(result.Scheme)
+	fmt.Println(result.Host)
+	fmt.Println(result.Path)
+	fmt.Println(result.Port())
+	fmt.Println(result.RawQuery)
+
+	qparams := result.Query()
+	fmt.Printf("The type of query params are : %T\n", qparams)
+
+	fmt.Println(qparams["coursename"])
+
+	for _, val := range qparams {
+		fmt.Println("Param is :", val)
+	}
+
+	partsOfUrl := &url.URL{
+		Scheme:  "https",
+		Host:    "lco.dev",
+		Path:    "/tutcss",
+		RawPath: "user=hitesh",
+	}
+
+	anotherURL := partsOfUrl.String()
+	fmt.Println(anotherURL)
+}
